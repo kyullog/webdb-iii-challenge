759af8c1bea1c4c6e5845706490716b93ff8cbd2
Jonathan Dorety <kyullog@gmail.com>
2019-03-19 22:17:07 -0400

    Make knex configuration modular

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const helmet = require("helmet");
-const knex = require("knex");
-const knexConfig = require("./data/knexfile");
-const db = knex(knexConfig.development);
+const cohortsRouter = require("./routers/cohortsRouter");
 
 const server = express();
 const protection = helmet();
 const parser = express.json();
 server.use(protection, parser);
+
+server.use(`/api/cohorts`, cohortsRouter);
 
 module.exports = server;
