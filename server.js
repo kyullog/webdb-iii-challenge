dc2058d9bdceeb2ca6cebc929080d71717449f9b
Jonathan Dorety <kyullog@gmail.com>
2019-03-19 21:35:59 -0400

    Clean up

--- server.js
+++ server.js
@@ -2,5 +2,8 @@ const express = require("express");
 const helmet = require("helmet");
 
 const server = express();
+const helmet = helmet();
+const parser = express.json();
+server.use(helmet, parser);
 
 module.exports = server;
